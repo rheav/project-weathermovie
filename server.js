9aa8b41ea764380d903218eb542cb9c181e490de
Victor Rhéa <rheav7@gmail.com>
2023-11-08 00:13:48 -0300

    backend route fixed

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const fetch = require("node-fetch"); // Added fetch library
 const app = express();
 require("dotenv").config();
 
